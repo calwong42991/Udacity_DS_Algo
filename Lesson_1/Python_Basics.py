ea19262300adb6967915bea02eb3cb67bdb9bdda
Calvin Wong <calwong42991@gmail.com>
2018-09-11 21:27:14 -0700

    Quiz 1/2 completed

---
+++ Lesson_1/Python_Basics.py
@@ -0,0 +1,7 @@
+# Data Types
+boolean = True
+number = 1.1
+string = "Strings can be declared with single or double quotes"
+list = ["lists can have", 1, 2, 3, 4, "or more types together"]
+tuple = ("Tuples", "can have", "more than", 2, 2, "elements")
+dictionary = {'One': 1, 'Two': 2, 'Three': 3}
